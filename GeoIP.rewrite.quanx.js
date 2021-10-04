92321af81eb460f6784c166d15b014c2c74d9065
RainyMoment <60817730+RainyMoment@users.noreply.github.com>
2021-10-04 10:27:19 +0800

    Update GeoIP.rewrite.quanx.js

--- GeoIP.rewrite.quanx.js
+++ GeoIP.rewrite.quanx.js
@@ -1,6 +1,6 @@
 [task_local]
 
 // GeoIP查询 - ip.sb
-event-interaction https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/geo_location.js, tag=查询, img-url=location.fill.viewfinder.system, enabled=true
+https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/geo_location.js
 
 
